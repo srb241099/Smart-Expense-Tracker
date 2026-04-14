f363f265aaebad1f884daa5c03948865e420cfb0
srb241099 <136821168+srb241099@users.noreply.github.com>
2026-04-14 12:21:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
+const CACHE = 'expense-flow-v1';
+const FILES = ['./', './index.html'];
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('app-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html'
-      ]);
-    })
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
